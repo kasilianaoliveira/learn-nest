3ce18af0dad8c620da3e2db655295c858edbb88a
kasilianaoliveira <kasilianaoliveira@gmail.com>
2025-05-03 16:08:00 -0300

    feat: add basic flow

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,11 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Patch,
+	Post,
+	Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
 	constructor(private readonly taskService: TasksService) {}
 	@Get()
-	getTasks() {
-		return this.taskService.getTasks();
+	findAllTasks(@Query() queryParams: any) {
+		console.log(queryParams);
+		return this.taskService.findAll();
+	}
+
+	//pode ficar tbm assim: @Param('id') id: string)
+	//@Param() params: { id: string })
+	@Get(':id')
+	findOneTask(@Param('id') id: number) {
+		console.log(typeof id);
+		return this.taskService.findOne(Number(id));
+	}
+
+	@Post()
+	createTask(@Body() body: any) {
+		console.log(body);
+
+		return this.taskService.create(body);
+	}
+
+	@Patch(':id')
+	updateTask(@Param('id') id: string, @Body() body: any) {
+		return this.taskService.update(Number(id), body);
+	}
+
+	@Delete(':id')
+	deleteTask(@Param('id') id: string) {
+		return this.taskService.delete(Number(id));
 	}
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,17 +1,68 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Task, TaskInput } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
-	getTasks() {
-		return [
-			{
-				id: 1,
-				task: 'Comprar pão',
-			},
-			{
-				id: 2,
-				task: 'Comprar queijo',
-			},
-		];
+	private readonly tasks: Task[] = [
+		{
+			id: 1,
+			name: 'sujeito programador',
+			description: 'Minha descrição',
+			completed: false,
+		},
+		{
+			id: 2,
+			name: 'comprar pão',
+			description: 'Minha descrição',
+			completed: false,
+		},
+	];
+	findAll() {
+		const tasks = this.tasks;
+		if (tasks.length > 0) return tasks;
+
+		throw new HttpException('Tasks not found', HttpStatus.NOT_FOUND);
+	}
+
+	findOne(id: number) {
+		const findItem = this.tasks.find((item) => item.id === id);
+		if (findItem) return findItem;
+
+		throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+	}
+
+	create(body: TaskInput) {
+		const newId = this.tasks.length + 1;
+		const newTask = {
+			id: newId,
+			...body,
+		};
+		this.tasks.push(newTask);
+		return this.tasks;
+	}
+
+	delete(id: number) {
+		const indexTaskId = this.tasks.findIndex((item) => item.id === id);
+
+		if (indexTaskId !== -1) {
+			this.tasks.splice(indexTaskId, 1);
+		}
+		return this.tasks;
+	}
+
+	update(id: number, body: TaskInput) {
+		const taskIndex = this.tasks.findIndex((task) => task.id === id);
+
+		if (taskIndex < 0) {
+			throw new HttpException('Task not found', HttpStatus.NOT_FOUND);
+		}
+		const taskItem = this.tasks[taskIndex];
+
+		this.tasks[taskIndex] = {
+			...taskItem,
+			...body,
+		};
+
+		return 'Tarefa atualizada com sucesso';
 	}
 }
