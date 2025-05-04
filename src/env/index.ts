a6e132544a55d144a965a4b34c1deda5def0f44e
kasilianaoliveira <kasilianaoliveira@gmail.com>
2025-05-04 15:56:06 -0300

    feat: add database connection using prisma

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,9 @@ import { z } from 'zod';
 const envSchema = z.object({
 	NODE_ENV: z.enum(['dev', 'test', 'production']),
 	PORT: z.coerce.number().default(3333),
+	POSTGRES_USER: z.string(),
+	POSTGRES_PASSWORD: z.string(),
+	POSTGRES_DB: z.string(),
 });
 
 const _env = envSchema.safeParse(process.env);
